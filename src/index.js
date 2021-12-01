e7c7472b714374bce68665cd0370dbe8bf04a4d0
Federico Meza <mezafabrello@gmail.com>
2021-12-01 19:56:08 -0300

    Allow all origins and methods

--- src/index.js
+++ src/index.js
@@ -8,20 +8,7 @@ const app = express();
 
 //Middlewares
 
-const allowedOrigins = ['http://localhost:3000', 'https://passwd.zaifo.com.ar'];
-
-app.use(cors({
-	origin: (origin, callback) => {
-		if (!origin) return callback(null, true);
-		if (allowedOrigins.indexOf(origin) === -1) {
-			var msg = `The CORS policy for this site does not allow access from the specified origin (${origin}).`;
-			return callback(new Error(msg), false);
-		}
-		return callback(null, true);
-	},
-	methods: ['GET', 'POST'],
-	credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 
